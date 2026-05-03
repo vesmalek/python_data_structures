37cfacab947c50e4590cb1ce5143cf574b4e1306
Ismail Malik <vesmalick@gmail.com>
2026-05-03 17:42:49 +0300

    Solved question 01 to 04 of python lists

--- exercise.py
+++ exercise.py
@@ -1,11 +1,21 @@
 # Qn 01. Create a list of 5 product names
 
+products = ["tomato", "orange", "apple", "banana", "cucumber"]
+
 # Qn 02. Print the last product using negative indexing
+
+print(products[-1])
 
 # Qn 03. Slice the middle 3 products
 
+print(products[1:4])
+
 # Qn 04. Append a new product, then remove the first one
 
+products.append("guava")
+print(products)
+products.pop(0)
+print(products)
 # Qn 05. Create a tuple of (product_name, price, in_stock) for one product
 
 # Qn 06. Unpack that tuple into 3 variables and print each
