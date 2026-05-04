f8e8f8b5e0afe661e96f668b851dbcf97d7da404
Ismail Malik <vesmalick@gmail.com>
2026-05-04 23:10:21 +0300

    Learned about tuples and did question 4 and 5 in Python Lists and Tuples

---
+++ tuples.py
@@ -0,0 +1,11 @@
+# 5. Create a tuple of (product_name, price, in_stock) for one product
+
+product = ("Apple", 3.99, True)
+
+# 6. Unpack that tuple into 3 variables and print each
+
+product_name, price, in_stock = product
+
+print(product_name)
+print(price)
+print(in_stock)
