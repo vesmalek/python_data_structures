f8fc84ad75120328edf749f8f9be3dd713dbc4fe
Ismail Malik <vesmalick@gmail.com>
2026-05-08 12:18:47 +0300

    Learning Python map() function

---
+++ map.py
@@ -0,0 +1,18 @@
+# map() function in Python , map, zip, enumerate, filter (map and filter ni older versions za list comprehensions)
+
+prices = [34, 12, 20, 35, 55, 60]
+
+def discount(price):
+    return price * 0.5
+
+discounted = list(map(lambda price: price * 0.5, prices))
+print()
+print(discounted)
+
+discounted2 = list(map(discount, prices))
+print()
+print(discounted2)
+
+discounted3 = [price * 0.5 for price in prices]
+print()
+print(discounted3)
